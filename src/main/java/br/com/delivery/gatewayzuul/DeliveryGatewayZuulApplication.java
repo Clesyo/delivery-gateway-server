905566c21c082c5b9f6cf3417b53cd7499a4c034
clesyo.silva <clesyo.silva@maidahealth.com>
2022-01-24 16:23:40 -0300

    Commit inicial

---
+++ src/main/java/br/com/delivery/gatewayzuul/DeliveryGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package br.com.delivery.gatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryGatewayZuulApplication.class, args);
+	}
+
+}
